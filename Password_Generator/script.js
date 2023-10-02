4d40e726978a06c4d4c90a61335738e12555d938
vinod6535 <vinodsaini6535@gmail.com>
2023-10-03 00:40:44 +0530

    Adding Password Generator Website

---
+++ Password_Generator/script.js
@@ -0,0 +1,21 @@
+const inputSlider = document.querySelector("[data-lengthSlider]");
+const lengthDisplay = document.querySelector("[data-lengthNumber]");
+const passwordDisptay = document.querySelector(" [data—passwordDisplay)");
+const copyBtn = document.querySetector(" [data—copy]");
+const copyMsg = document.querySetector(" [data—copyMsg]");
+const uppercaseCheck = document.querySelector("#uppercase");
+const lowercaseCheck = document.querySelector("#lowercase");
+const numberCheck = document.querySelector("#numbers");
+const symbolCheck = document.querySelector("#symbols");
+const indicator = document. querySetector(" [data-indicator]");
+const generateBtn = document.querySelector(".generateButton");
+const allCheckBox = document.querySelector("input[type=checkbox]");
+
+// Default Values
+
+let passwor = "";
+let passwordLength = 10;
+let checkCount = 1;
+
+
+
