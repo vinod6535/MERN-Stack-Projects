f0859b221945bf3d660d26ac223f6788feb62f2e
Vinod Saini <vinodsaini6535@gmail.com>
2023-10-20 12:44:32 +0530

    Adding update button functanility (JS)

--- Employee (CRUD) Management System/index.js
+++ Employee (CRUD) Management System/index.js
@@ -1,93 +1,111 @@
-// Add employee btn 
-var addBtn = document.getElementById("add-emp");
+
+// Open and close Modal
+var addBtn = document.querySelector("#add-btn");
 var modal = document.querySelector(".modal");
-var closeModalBtn = document.querySelector(".close-icon");
-var registerbtn = document.querySelector("#register-btn");
+var closeBtn = document.querySelector(".close-icon");
 
-
-addBtn.onclick = function(){
-    modal.classList.add("active");
-} 
-
-closeModalBtn.onclick = function(){
-    modal.classList.remove("active");
-} 
-
-var imgUrl;
-
-// Start of all Global variable
-var id = document.querySelector("#id");
-var f_name = document.querySelector("#name");
-var l_name = document.querySelector("#l-name");
-var email= document.querySelector("#email");
-var office_code = document.querySelector("#office-code");
-var job_title = document.querySelector("#job-title"); 
-var userData = [];
+// To empty the input box after update the user data for new registration
 var registerForm = document.querySelector("#registration-form");
-// End of all Global variables
+var allInput = registerForm.querySelectorAll("input");
 
+// open Modal
+addBtn.onclick = ()=>{
+    modal.classList.add("active"); 
+}
 
-registerForm.onsubmit = (e) => {
-    registerData();
-    registerForm.reset();
-    getDataFromLocal();
-    // To prevent the page reload after registration (Default behavior)
+// Close Modal
+closeBtn.onclick=()=>{
+    modal.classList.remove("active");
+    // var i;
+    // for(i=0;i<allInput.length;i++){
+    //     allInput[i].value="";
+    // }
+}
+
+// Start all global Variables
+
+var userData=[];
+var idEl= document.querySelector("#id");
+var nameEl = document.querySelector("#name");
+var l_nameEl = document.querySelector("#l-name");
+var emailEl = document.querySelector("#email");
+var  officeEl = document.querySelector("#office-code");
+var jobTitleEl = document.querySelector("#job-title");
+var imgUrl;
+var registerBtn = document.getElementById("register-btn");
+var updateBtn = document.getElementById("update-btn");
+var profile_pic = document.querySelector("#profile-pic");
+var uploadPic = document.querySelector("#upload-pic");
+
+// End of all global Variables
+
+registerBtn.onclick = (e) => {
     e.preventDefault();
-    closeModalBtn.click();
+    // alert();
+    registrationData();
+    getDataFromLocal();
+    registerForm.reset();
+    // So that the page does not refrest after the registration
+    closeBtn.click();  
+
+}
+// To add the local stroge data to array , so after refresh we can't lost data
+
+if(localStorage.getItem("userData") != null){
+    userData = JSON.parse(localStorage.getItem("userData"));
 }
 
-
-function registerData(){
+function registrationData(){
     userData.push({
-        id : id.value,
-        name : f_name.value, 
-        l_name  : l_name.value,
-        email : email.value,
-        of_code : office_code.value,
-        job_title :job_title.value,
-        profile_pic : imgUrl == undefined ? "./img/default.avif" : imgUrl
+        id : idEl.value,
+        name : nameEl.value,
+        l_name : l_nameEl.value,
+        email : emailEl.value,
+        office : officeEl.value,
+        jobTitle : jobTitleEl.value,
+        profilePic : imgUrl == undefined ? "./img/default.avif" : imgUrl
     })
-    var userStringData = JSON.stringify(userData);
-    localStorage.setItem("userStringDataKey",userStringData);
-    Swal.fire( 'Good job!','Registration Successfull','success')
-}
-// To store the data in the array as a object and the data remains save after refresh the page
-if(localStorage.getItem("userStringDataKey")!=null){
-    userData = JSON.parse(localStorage.getItem("userStringDataKey"));
+    var userString = JSON.stringify(userData);
+    localStorage.setItem("userData",userString);
+    Swal.fire('Good job!','Registration Successful','success');
 }
 
-// Starting return data on page from local stroge
+
+// return data on page from local stroge
 
 var tableData = document.querySelector("#table-data");
- 
 
 // ...
 
-const getDataFromLocal = () => {
+// In the getDataFromLocal function, you're adding event listeners for delete buttons.
+// You need to wrap the delete button handling code within the forEach loop.
+function getDataFromLocal() {
     tableData.innerHTML = "";
     userData.forEach((data, index) => {
-        const row = document.createElement("tr");
-        row.setAttribute("index", index);
-        row.innerHTML = `
-            <td>${index + 1}</td>
-            <td><img src="${data.profile_pic}" alt="Employee" width="50" height="50" style="border-radius: 50%;"></td>
-            <td>${data.id}</td>
-            <td>${data.name}</td>
-            <td>${data.l_name}</td>
-            <td>${data.email}</td>
-            <td>${data.of_code}</td>
-            <td>${data.job_title}</td>
-            <td>
-                <button><i class="fa fa-eye"></i></button>
-                <button class="del-btn" style="background-color: #EE534F;"><i class="fa fa-trash"></i></button>
-            </td>
+        tableData.innerHTML += `
+      <tr data-index="${index}">
+        <td>${index + 1}</td>
+        <td><img src="${data.profilePic}" alt="Employee" width="50" height="50" style="border-radius: 50%;"></td>
+        <td>${data.id}</td>
+        <td>${data.name}</td>
+        <td>${data.l_name}</td>
+        <td>${data.email}</td>
+        <td>${data.office}</td>
+        <td>${data.jobTitle}</td>
+        <td>
+            <button class="edit-btn"><i class="fa fa-eye"></i></button>
+            <button class="del-btn" style="background-color: #EE534F;"><i class="fa fa-trash"></i></button>
+        </td>
+    </tr>
         `;
+    });
 
-        const delButton = row.querySelector(".del-btn");
+    // Delete User Data
+
+    // Add event listeners to the delete buttons within the forEach loop
+    var allDelBtn = document.querySelectorAll(".del-btn");
+    allDelBtn.forEach((delButton, index) => {
         delButton.onclick = () => {
-            // Get the index from the row's attribute
-            const dataIndex = row.getAttribute("index");
-
             // Show a confirmation dialog
             Swal.fire({
                 title: 'Are you sure?',
@@ -100,37 +118,76 @@ const getDataFromLocal = () => {
             }).then((result) => {
                 if (result.isConfirmed) {
                     // User confirmed the deletion
-                    deleteUser(dataIndex);
+                    deleteUser(index);
                 }
             });
         };
-
-        tableData.appendChild(row);
     });
+ 
 
-    // ...
+    // Update User Data
 
-    function deleteUser(index) {
-        // Remove the corresponding user data
-        userData.splice(index, 1);
-        // Update the localStorage
-        localStorage.setItem("userStringDataKey", JSON.stringify(userData));
-        // Refresh the displayed data
-        getDataFromLocal();
-        // Display a success message
-        Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
-    }
+    var allEdit = document.querySelectorAll(".edit-btn");   
+
+for (let i = 0; i < allEdit.length; i++) {
+    allEdit[i].addEventListener("click", function() {
+        var tr = this.parentElement.parentElement;
+        var td = tr.getElementsByTagName("TD");
+        var index = tr.getAttribute("data-index");
+        var imgTag = td[1].getElementsByTagName("IMG");
+        var profilePic = imgTag[0].src;
+        // alert(profile_pic);
+        var id = td[2].innerHTML;
+        var name = td[3].innerHTML;
+        var l_name = td[4].innerHTML;
+        var email = td[5].innerHTML;
+        var officeCode = td[6].innerHTML;
+        var jobTitle = td[7].innerHTML;
+
+        addBtn.click();
+
+        registerBtn.disabled= true;
+        updateBtn.disabled = false;
+
+            idEl.value= id;
+            nameEl.value = name;
+            l_nameEl.value = l_name;
+            emailEl.value = email;
+            officeEl.value= officeCode;
+            jobTitleEl.value = jobTitle;
+            profile_pic.src = profilePic;
+            updateBtn.onclick=function(e){
+                userData[index]={
+                    id : idEl.value,
+                    name : nameEl.value,
+                    l_name : l_nameEl.value,
+                    email : emailEl.value,
+                    office : officeEl.value,
+                    jobTitle : jobTitleEl.value,
+                    profilePic : uploadPic.value == "" ? profile_pic.src : imgUrl
+                }
+                localStorage.setItem("userData",JSON.stringify(userData));
+            }
+    });
 }
 
+}
 
-// ...
+function deleteUser(index) {
+    // Remove the corresponding user data
+    userData.splice(index, 1);
+    // Update the localStorage
+    localStorage.setItem("userData", JSON.stringify(userData));
+    // Refresh the displayed data
+    getDataFromLocal();
+    // Display a success message
+    Swal.fire('Deleted!', 'Your data has been deleted.', 'success');
+}
 
 getDataFromLocal();
 
 // Image Processing
 
-var profilePic = document.querySelector("#profile-pic");
-var uploadPic = document.querySelector("#upload-pic");
 
 uploadPic.onchange = ()=>{
 
@@ -138,8 +195,8 @@ uploadPic.onchange = ()=>{
             var fReader = new FileReader();
             fReader.onload=function(e){
                 imgUrl = e.target.result;
-                profilePic.src= imgUrl;
-            }
+                profile_pic.src= imgUrl;
+            } 
             fReader.readAsDataURL(uploadPic.files[0]);
         }
         else{
